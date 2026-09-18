51079f5ece53854918051df102e87c38747a50dc
luojiaguwu2004 <119292324+luojiaguwu2004@users.noreply.github.com>
2026-09-18 10:59:44 +0800

    Update build-harbor-aarch64.sh

--- build-harbor-aarch64.sh
+++ build-harbor-aarch64.sh
@@ -6,7 +6,7 @@ git clone https://github.com/alanpeng/harbor-arm.git
 # Replace dev-arm image tag
 sed -i "s#dev-arm#${GIT_BRANCH}-aarch64#g" harbor-arm/Makefile
 
-# execute build command：Download harbor source code
+# execute build command: Download harbor source code
 cd harbor-arm
 git clone --branch ${GIT_BRANCH} https://github.com/goharbor/harbor.git src/github.com/goharbor/harbor
 cp -f ../harbor/Makefile src/github.com/goharbor/harbor/
@@ -22,11 +22,14 @@ make compile_redis
 # Prepare to build arm architecture image data:
 make prepare_arm_data
 
-# Replace build arm image parameters：
+# Replace build arm image parameters:
 make pre_update
 
 # Compile harbor components:
 make compile COMPILETAG=compile_golangimage
+
+# ========== 新增这一行：构建基础镜像 harbor-prepare-base ==========
+make prepare-base
 
 # Build harbor arm image:
 make build GOBUILDTAGS="include_oss include_gcs" BUILDBIN=true TRIVYFLAG=true GEN_TLS=true PULL_BASE_FROM_DOCKERHUB=false
